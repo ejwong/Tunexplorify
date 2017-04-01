e5f4d8daadb15a6f6a0314c4320fe4d7f9c0b525
ejwong <emily.wong8899@gmail.com>
2017-03-31 20:12:36 -0700

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,5 @@
+#from Spotify
+# you will need to create an app at https://developer.spotify.com/my-applications/#!/applications
+
+CLIENT_SECRET = "2ac1cee0ab7f4472b0ba9e7a5a3c4061"
+CLIENT_ID = "b38ec749b35a4969a50e45af1d4bb07d"
